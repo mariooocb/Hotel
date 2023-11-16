255d5f93d356407d14aa6307115d9dc19f050aae
Mariooocb <porminombre165@gmail.com>
2023-11-16 17:43:49 -0500

    Areglando estructura del proyecto

--- src/main/com/hotel/service/GuestFactory.java
+++ src/main/com/hotel/service/GuestFactory.java
@@ -1,17 +1,25 @@
-package com.hotel.hotel.service;
+package com.hotel.service;
 
-import com.hotel.hotel.dto.GuestRequest;
-import com.hotel.hotel.model.GuestEntity;
-import org.springframework.stereotype.Component;
 
-@Component
+/*import com.hotel.dto.GuestRequest;
+import com.hotel.model.GuestEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Component*/
 public class GuestFactory {
-    public void createGuest(GuestRequest guestRequest) {
+
+    /*@Autowired
+    private GuestFactory guestFactory;
+
+    public  GuestEntity createGuest(GuestRequest guestRequest) {
         GuestEntity guestEntity = new GuestEntity();
         guestEntity.setName(guestRequest.getName());
         guestEntity.setLastName(guestRequest.getLastName());
         guestEntity.setDateOfBirth(guestRequest.getDateOfBirth());
         guestEntity.setNationality(guestRequest.getNationality());
         guestEntity.setPhone(guestRequest.getPhone());
-    }
+        return GuestEntity;
+    }*/
+
 }
